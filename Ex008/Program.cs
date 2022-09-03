f62b659ec9777e0cc984d77e7c6faa1e7d3a6c10
Yury <letelmimo@gmail.com>
2022-09-03 14:45:45 +0300

    Special greeting

---
+++ Ex008/Program.cs
@@ -0,0 +1,13 @@
+﻿// Salutation
+Console.Write("Enter Username:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+  Console.Write("Hooray, it's Masha!");  
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
