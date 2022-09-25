ad3847d90923402e216283941f7634c60ac2dc28
Yury <letelmimo@gmail.com>
2022-09-25 15:55:12 +0300

    Recursion Factorial Fibonacci

---
+++ RecursionFactorial/Program.cs
@@ -0,0 +1,43 @@
+﻿// int Factorial(int n)
+// {
+//     if (n ==1) return 1;
+//     else return n*Factorial(n-1);
+// }
+// Console.WriteLine(Factorial(5));
+double Fibonacci(int n)
+{
+  if(n == 1 || n == 2) return 1;
+  else return Fibonacci(n-1) + Fibonacci(n-2);  
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
