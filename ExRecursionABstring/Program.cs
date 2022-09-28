399086826d5e295b3151d376a4378cd7c39a2df6
Yury <letelmimo@gmail.com>
2022-09-28 11:39:49 +0300

    Simle recursion example

---
+++ ExRecursionABstring/Program.cs
@@ -0,0 +1,18 @@
+﻿string NumbersFor(int a,int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b;i++)
+
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRecursion(int a, int b)
+{
+    if (a<=b) return $"{a} "+ NumbersRecursion(a+1,b);
+    else return string.Empty;
+}
+Console.WriteLine(NumbersFor(1,10));
+Console.WriteLine(NumbersRecursion(1,10));
